712a5d0663d4c144fbfac473b3423feb6a369b78
StephenArg <stephena9871@gmail.com>
2025-12-03 12:50:59 +0100

    Fix format size error

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -12,8 +12,11 @@ const FileUpload = ({ onSuccess, onError, onLoadingChange, onUploadProgress }) =
     const k = 1024;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return Math.round(bytes / Math.pow(k, i) * 100) / 100 + ' ' + sizes[i];
+    return Math.round((bytes / Math.pow(k, i)) * 100) / 100 + ' ' + sizes[i];
   };
+
+  // Alias for consistency with other components
+  const formatBytes = formatFileSize;
 
   const uploadFileWithProgress = (file, index, totalFiles) => {
     return new Promise((resolve, reject) => {
@@ -36,9 +39,9 @@ const FileUpload = ({ onSuccess, onError, onLoadingChange, onUploadProgress }) =
           const timeDelta = (now - lastUpdateTime) / 1000;
           const loadedDelta = e.loaded - lastLoaded;
           const speed = timeDelta > 0 && loadedDelta > 0 
-            ? formatBytes(loadedDelta / timeDelta) + '/s' 
+            ? formatFileSize(loadedDelta / timeDelta) + '/s' 
             : elapsed > 0 
-              ? formatBytes(e.loaded / elapsed) + '/s' 
+              ? formatFileSize(e.loaded / elapsed) + '/s' 
               : '';
           
           lastUpdateTime = now;
